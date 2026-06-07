d764b94307d935574062fdfe6693c3bd60b1175d
Alexander Tverskoy <astverskoy@gmail.com>
2026-06-07 09:07:25 +0300

    Update donation form with new links

--- src/formdonate.pas
+++ src/formdonate.pas
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------------
-//  PadXml © 2024 by Alexander Tverskoy
+//  Trayslate © 2026 by Alexander Tverskoy
 //  Licensed under the GNU General Public License, Version 3 (GPL-3.0)
 //  You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.html
 //-----------------------------------------------------------------------------------
@@ -12,48 +12,32 @@ interface
 
 uses
   Forms,
+  Classes,
   StdCtrls,
   Buttons,
   Clipbrd,
-  LCLIntf, Classes;
+  Graphics,
+  LCLIntf, ExtCtrls;
 
 type
 
   { TformDonatePadXml }
 
   TformDonatePadXml = class(TForm)
-    buttonOk: TButton;
-    editWallet1: TEdit;
-    buttonCopy1: TSpeedButton;
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
-    editWallet2: TEdit;
-    buttonCopy2: TSpeedButton;
-    Label4: TLabel;
-    Label5: TLabel;
-    editWallet3: TEdit;
-    buttonCopy3: TSpeedButton;
-    Label6: TLabel;
-    Label7: TLabel;
-    editWallet4: TEdit;
-    buttonCopy4: TSpeedButton;
-    Label8: TLabel;
-    Label9: TLabel;
-    labelUrl: TLabel;
-    Label10: TLabel;
-    labelUrl1: TLabel;
-    procedure buttonCopy1Click(Sender: TObject);
-    procedure buttonCopy2Click(Sender: TObject);
-    procedure buttonCopy3Click(Sender: TObject);
-    procedure buttonCopy4Click(Sender: TObject);
-    procedure labelUrlClick(Sender: TObject);
-    procedure editWallet4Click(Sender: TObject);
-    procedure editWallet3Click(Sender: TObject);
-    procedure editWallet1Click(Sender: TObject);
-    procedure editWallet2Click(Sender: TObject);
+    BtnOk: TButton;
+    ImageBank: TImage;
+    ImageCrypto: TImage;
+    LabelDonate: TLabel;
+    LabelSupport: TLabel;
+    LabelContribution: TLabel;
+    labelBank: TLabel;
+    labelCrypto: TLabel;
+    procedure labelBankClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
+
   public
+
   end;
 
 var
@@ -67,57 +51,17 @@ uses systemtool;
 
   { TformDonatePadXml }
 
-procedure TformDonatePadXml.buttonCopy1Click(Sender: TObject);
+procedure TformDonatePadXml.FormCreate(Sender: TObject);
 begin
-  if editWallet1.CanFocus then  editWallet1.SetFocus;
-  editWallet1.SelectAll;
-  Clipboard.AsText := editWallet1.Text;
+  ApplicationTranslate(language, self);
+
+  //labelBank.Font.Color := ThemeColor(clBlue, clSkyBlue);
+  //labelCrypto.Font.Color := ThemeColor(clBlue, clSkyBlue);
 end;
 
-procedure TformDonatePadXml.buttonCopy2Click(Sender: TObject);
-begin
-  if editWallet2.CanFocus then  editWallet2.SetFocus;
-  editWallet2.SelectAll;
-  Clipboard.AsText := editWallet2.Text;
-end;
-
-procedure TformDonatePadXml.buttonCopy3Click(Sender: TObject);
-begin
-  if editWallet3.CanFocus then  editWallet3.SetFocus;
-  editWallet3.SelectAll;
-  Clipboard.AsText := editWallet3.Text;
-end;
-
-procedure TformDonatePadXml.buttonCopy4Click(Sender: TObject);
-begin
-  if editWallet4.CanFocus then  editWallet4.SetFocus;
-  editWallet4.SelectAll;
-  Clipboard.AsText := editWallet4.Text;
-end;
-
-procedure TformDonatePadXml.labelUrlClick(Sender: TObject);
+procedure TformDonatePadXml.labelBankClick(Sender: TObject);
 begin
   OpenUrl((Sender as TLabel).Hint);
-end;
-
-procedure TformDonatePadXml.editWallet4Click(Sender: TObject);
-begin
-  editWallet4.SelectAll;
-end;
-
-procedure TformDonatePadXml.editWallet3Click(Sender: TObject);
-begin
-  editWallet3.SelectAll;
-end;
-
-procedure TformDonatePadXml.editWallet1Click(Sender: TObject);
-begin
-  editWallet1.SelectAll;
-end;
-
-procedure TformDonatePadXml.editWallet2Click(Sender: TObject);
-begin
-  editWallet2.SelectAll;
 end;
 
 end.
